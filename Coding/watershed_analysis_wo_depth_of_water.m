a8d818eb7518fbebaf909088f07269f5569f4e33
Anders Voldsund <andersvoldsund@gmail.com>
2016-02-29 15:45:11 +0100

    Made some changes to what metadata the plot has

--- Coding/watershed_analysis_wo_depth_of_water.m
+++ Coding/watershed_analysis_wo_depth_of_water.m
@@ -1,6 +1,10 @@
 %% Read the height of an area. Plot the height of the landscape
-I = GEOTIFF_READ('C:\Users\Anders O. Voldsund\Dropbox\watershed\Matlab examples\Data\anders_hoh.tiff');
+I = GEOTIFF_READ('anders_hoh.tiff');
 
 % Flip y axis so image is not mirrored up-down
-% caxis shows the range of the heights
-im = imshow(I.z, 'xdata', I.x, 'ydata', fliplr(I.y)); caxis([0, 1200]), colorbar
+% caxis shows the range of the height
+im = imshow(I.z, 'xdata', I.x, 'ydata', fliplr(I.y)); caxis([0, 800]), colorbar
+title('Height of the area')
+axis on
+xlabel('Meters')
+ylabel('Meters')
