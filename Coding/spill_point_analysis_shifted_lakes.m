fd1128970a63d10efa946754f2625ae2aef469e4
Anders Voldsund <andersvoldsund@gmail.com>
2016-03-04 13:32:19 +0100

    Move plotting function out of function

--- Coding/spill_point_analysis_shifted_lakes.m
+++ Coding/spill_point_analysis_shifted_lakes.m
@@ -8,19 +8,18 @@ ds_fac = 32;
 [I_lakes, G_lakes, zvals_lakes] = add_tiff_data('anders_innsjo.tiff', ds_fac);
 
 % Do some processing to make sure that the matrices are correct
+
 % Make sure that anders_innsjo is a boolean matrix
-zvals_lakes = make_matrix_boolean(zvals_lakes);
+zvals_lakes = helpFunctions.make_matrix_boolean(zvals_lakes);
 % Interpolate all heights which are clearly wrong
-zvals_landscape = interpolate_extreme_values(0, 2469, zvals_landscape);
+zvals_landscape = helpFunctions.interpolate_extreme_values(0, 2469, zvals_landscape);
 
 % Lower terrain for all lakes and make sure the heights are positive 
 zvals_landscape = zvals_landscape - double(zvals_lakes) * 20;
 zvals_landscape = max(0, zvals_landscape);
 
-
 % Make the input for the trapAnalysis function
 zvals_landscape = zvals_landscape(:);
-
 G_landscape.nodes.coords(:,3) = [zvals_landscape; ones(size(zvals_landscape)) * max(zvals_landscape) + 1];
 G_landscape = mcomputeGeometry(G_landscape);
 Gt = topSurfaceGrid(G_landscape);
@@ -28,15 +27,4 @@ Gt = topSurfaceGrid(G_landscape);
 % performing spill point analysis
 tic; ts = trapAnalysis(Gt, false); toc
 
-% show map
-plotCellData(Gt, Gt.cells.H, 'edgecolor', 'none');
-cmap = flipud(copper);
-colormap(cmap);
-set(gca, 'zdir', 'normal'); colorbar;
-
-% Plot all trap cells (i.e. "lake cells")
-hold on;
-plotGrid(extractSubgrid(Gt, find(ts.traps~=0)));
-title('Plot of trap cells')
-xlabel('Meters')
-ylabel('Meters')
+plot.plot_trap_analysis(Gt, ts);
