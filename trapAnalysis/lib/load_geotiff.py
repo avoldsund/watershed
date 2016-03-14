58f6bb16642b2927af7aab67974907bef47a93d7
Anders Voldsund <andersvoldsund@gmail.com>
2016-03-14 19:57:06 +0100

    Read geotiff file and save to metadata object and node collection object

--- trapAnalysis/lib/load_geotiff.py
+++ trapAnalysis/lib/load_geotiff.py
@@ -2,10 +2,16 @@ from osgeo import gdal  # For reading tiff files
 import numpy as np      # For masked matrices
 import util
 
-def load_geotiff_as_masked_array(filename, band = 1):
+
+def load_geotiff_as_masked_array(filename, band=1):
 
     data_set = gdal.Open(filename)
 
+    if data_set is None:
+        print "Error retrieving data set"
+        return
+
+    # Get number of grid points in the plane, as well as their domains
     geo_transform = data_set.GetGeoTransform()
     x_res = data_set.RasterXSize
     y_res = data_set.RasterYSize
@@ -14,11 +20,19 @@ def load_geotiff_as_masked_array(filename, band = 1):
     x_max = x_min + geo_transform[1] * x_res
     y_min = y_max + geo_transform[5] * y_res
 
+    landscape_metadata = util.LandscapeMetadata(x_res, y_res, x_min, x_max, y_min, y_max)
+
+    # Get data from selected band and remove invalid data points
     band = data_set.GetRasterBand(band)
     arr = np.ma.masked_array(band.ReadAsArray())
 
-    #Remove no-data values
     no_data_value = band.GetNoDataValue()
-
-    if (no_data_value):
+    print no_data_value
+    if no_data_value:
         arr[arr == no_data_value] = np.ma.masked
+
+    node_collection = util.NodeCollection(arr)
+    print node_collection.coordinates[4000][4000]
+
+    # count number of masked elements in the array
+    # print np.ma.count_masked(arr)
