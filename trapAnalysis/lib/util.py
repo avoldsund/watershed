4c394d83e19aa4f07985ea00ea6ec01db66b9030
Anders Voldsund <andersvoldsund@gmail.com>
2016-03-28 16:51:21 +0200

    Add test to see if the step size differs in the x- and y-direction

--- trapAnalysis/lib/util.py
+++ trapAnalysis/lib/util.py
@@ -9,10 +9,12 @@ class Landscape:
     num_of_nodes_y = None
     total_number_of_nodes = None
 
-    x_coord_min = None
-    x_coord_max = None
-    y_coord_min = None
-    y_coord_max = None
+    x_min = None
+    x_max = None
+    y_min = None
+    y_max = None
+
+    step_size = None
 
     def __init__(self, geo_transform, nx, ny):
         self.num_of_nodes_x = nx
@@ -23,7 +25,13 @@ class Landscape:
         self.y_min = self.y_max + geo_transform[5] * self.num_of_nodes_y
         self.total_number_of_nodes = nx * ny
         self.coordinates = np.empty((self.total_number_of_nodes, 3))
-
+        step_size_x = geo_transform[1]
+        step_size_y = geo_transform[5]
+        unequal_step_size = (abs(step_size_x) != abs(step_size_y))
+        if unequal_step_size:
+            print 'The step size in the x- and y-direction is not equal'
+            return
+        self.step_size = step_size_x
 
     def get_node_neighbors(self, node_index):
         return None
