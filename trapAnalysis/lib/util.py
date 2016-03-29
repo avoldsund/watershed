21f848a75645f58ac4dde6c890b0f8a44af50efa
Anders Voldsund <andersvoldsund@gmail.com>
2016-03-29 18:27:31 +0200

    Move several methods out of landscalpe class, such as get_node_index.

--- trapAnalysis/lib/util.py
+++ trapAnalysis/lib/util.py
@@ -3,28 +3,27 @@ import numpy as np
 
 class Landscape:
 
-    coordinates = None
-
-    num_of_nodes_x = None
-    num_of_nodes_y = None
-    total_number_of_nodes = None
-
-    x_min = None
-    x_max = None
-    y_min = None
-    y_max = None
-
-    step_size = None
+    #coordinates = None
+    #num_of_nodes_x = None
+    #num_of_nodes_y = None
+    #total_number_of_nodes = None
+    #x_min = None
+    #x_max = None
+    #y_min = None
+    #y_max = None
+    #step_size = None
 
     def __init__(self, geo_transform, nx, ny):
+
         self.num_of_nodes_x = nx
         self.num_of_nodes_y = ny
         self.x_min = geo_transform[0]
         self.y_max = geo_transform[3]
-        self.x_max = self.x_min + geo_transform[1] * self.num_of_nodes_x
-        self.y_min = self.y_max + geo_transform[5] * self.num_of_nodes_y
+        self.x_max = self.x_min + geo_transform[1] * (self.num_of_nodes_x - 1)
+        self.y_min = self.y_max + geo_transform[5] * (self.num_of_nodes_y - 1)
         self.total_number_of_nodes = nx * ny
         self.coordinates = np.empty((self.total_number_of_nodes, 3))
+
         step_size_x = geo_transform[1]
         step_size_y = geo_transform[5]
         unequal_step_size = (abs(step_size_x) != abs(step_size_y))
@@ -33,6 +32,9 @@ class Landscape:
             return
         self.step_size = step_size_x
 
+    #def __init__(self):
+    #    self.num_of_nodes_x = None
+    #    self.num_of_nodes_y = None
 
     def get_node_neighbors(self, node_index):
         """
@@ -51,17 +53,20 @@ class Landscape:
         return valid_neighbors
 
 
-    def get_node_index(self, x_coord, y_coord):
-        """
-        Given a node in the 2d-grid we return the index in the 1d-array
-        :param x_coord: Coordinate in the x-direction
-        :param y_coord: Coordinate in the y-direction
-        :return node_index:
-        """
-        node_index = x_coord * self.num_of_nodes_x + y_coord + 1
 
-        return node_index
+def find_steepest_neighbors(self, node_index):
+    return None
 
 
-    def find_steepest_neighbors(self, node_index):
-        return None
+def get_node_index(x_coord, y_coord, num_of_nodes_x, num_of_nodes_y):
+    """
+    Given a node in the 2d-grid we return the index in the 1d-array
+    :param x_coord: Coordinate in the x-direction
+    :param y_coord: Coordinate in the y-direction
+    :param num_of_nodes_x: Number of grid points in the x-direction
+    :param num_of_nodes_y: Number of grid points in the y-direction
+    :return node_index: Index in the 1d-array
+    """
+    node_index = x_coord + num_of_nodes_x * (num_of_nodes_y - (y_coord + 1))
+
+    return node_index
