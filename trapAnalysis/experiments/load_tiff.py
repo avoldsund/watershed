aa4afa5acc54be872a5e0b5428561c9fa9b494cb
Anders Voldsund <andersvoldsund@gmail.com>
2016-04-07 19:30:18 +0200

    Added code to find downslope neighbors. Need to be revised

--- trapAnalysis/experiments/load_tiff.py
+++ trapAnalysis/experiments/load_tiff.py
@@ -2,17 +2,34 @@ import sys
 
 # Linux:
 sys.path.insert(0, '/home/shomea/a/anderovo/Dropbox/watershed/trapAnalysis/lib')
+sys.path.insert(0, '/home/shomea/a/anderovo/Dropbox/watershed/trapAnalysis/util')
 # Windows:
-#sys.path.insert(0, "C:\Users\Anders O. Voldsund\Dropbox\watershed\\trapAnalysis\lib")
+# sys.path.insert(0, "C:\Users\Anders O. Voldsund\Dropbox\watershed\\trapAnalysis\lib")
 
 import load_geotiff
+import util
+import numpy as np
+import trap_analysis
+import time
 
 # Linux:
 file_name = '/home/shomea/a/anderovo/Dropbox/watershed/trapAnalysis/lib/anders_hoh.tiff'
 # Windows:
-#file_name = 'C:\Users\Anders O. Voldsund\Dropbox\watershed\\trapAnalysis\lib\\anders_hoh.tiff'
+# file_name = 'C:\Users\Anders O. Voldsund\Dropbox\watershed\\trapAnalysis\lib\\anders_hoh.tiff'
 
-data_set = load_geotiff.load_data_set(file_name)
-arr = load_geotiff.get_array_from_band(data_set)
-landscape = load_geotiff.set_landscape(data_set)
-load_geotiff.construct_landscape_grid(arr, landscape)
+landscape = load_geotiff.get_landscape(file_name)
+print 'Done importing landscape'
+nx = landscape.num_of_nodes_x
+ny = landscape.num_of_nodes_y/4
+total = nx * ny
+print nx, ny, total
+downslope_neighbors = util.get_downslope_neighbors(nx, ny, landscape.coordinates[0:total, 2])
+print downslope_neighbors
+print 'Done neighbors: '
+print 'Start finding terminal nodes: '
+start = time.time()
+terminal_nodes = trap_analysis.get_downslope_minimums(landscape.num_of_nodes_x, landscape.num_of_nodes_y, downslope_neighbors)
+end = time.time()
+time_taken = end - start
+print time_taken
+print terminal_nodes
