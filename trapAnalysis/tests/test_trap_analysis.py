6da7f95d32360e49045fbbae2a9a5b5777711b64
Anders Voldsund <andersvoldsund@gmail.com>
2016-04-15 13:37:24 +0200

    Add test for endnodes

--- trapAnalysis/tests/test_trap_analysis.py
+++ trapAnalysis/tests/test_trap_analysis.py
@@ -4,7 +4,7 @@ import trap_analysis
 import numpy as np
 
 
-def test_get_downslope_minimums_one_min():
+def test_get_node_endpoints_one_min():
 
     num_of_cols = 3
     num_of_rows = 3
@@ -16,7 +16,7 @@ def test_get_downslope_minimums_one_min():
     assert np.array_equal(terminal_nodes, result_terminal_nodes)
 
 
-def test_get_downslope_minimums_two_mins():
+def test_get_node_endpoints_two_mins():
 
     num_of_cols = 3
     num_of_rows = 3
@@ -28,12 +28,24 @@ def test_get_downslope_minimums_two_mins():
     assert np.array_equal(terminal_nodes, result_terminal_nodes)
 
 
-def test_get_downslope_minimums_one_min_rectangular():
+def test_get_node_endpoints_one_min_rectangular():
 
     num_of_cols = 2
     num_of_rows = 3
     downslope_neighbors = np.array([3, 3, 5, 5, 5, -1])
     result_terminal_nodes = np.array([5, 5, 5, 5, 5, 5])
+
+    terminal_nodes = trap_analysis.get_node_endpoints(num_of_cols, num_of_rows, downslope_neighbors)
+
+    assert np.array_equal(terminal_nodes, result_terminal_nodes)
+
+
+def test_get_node_endpoints_four_mins_rectangular():
+
+    num_of_cols = 4
+    num_of_rows = 3
+    downslope_neighbors = np.array([1, 6, 6, 7, 5, 6, -1, -1, 9, 10, -1, -1])
+    result_terminal_nodes = np.array([6, 6, 6, 7, 6, 6, 6, 7, 10, 10, 10, 11])
 
     terminal_nodes = trap_analysis.get_node_endpoints(num_of_cols, num_of_rows, downslope_neighbors)
 
