4fa7b022c8a61e8fdf0ac1419af17957af732ccf
Anders Voldsund <andersvoldsund@gmail.com>
2016-04-15 13:33:05 +0200

    Moved Matlab coding folder

--- Coding/+helpFunctions/pad_matrix.m
+++
@@ -1,7 +0,0 @@
-function paddedMatrix = pad_matrix(xDim, yDim, generalMatrix)
-
-[sizeX, sizeY] = size(generalMatrix);
-paddedMatrix = zeros(xDim, yDim);
-paddedMatrix(1:sizeX, 1:sizeY) = generalMatrix;
-
-end
