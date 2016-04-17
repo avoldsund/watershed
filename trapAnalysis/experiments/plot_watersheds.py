0b7bf96a9f3aca9d7c3abd69fb4323f46bdf57ba
Anders Voldsund <andersvoldsund@gmail.com>
2016-04-17 17:23:32 +0200

    Alter methods for plotting local minimus and watersheds

--- trapAnalysis/experiments/plot_watersheds.py
+++ trapAnalysis/experiments/plot_watersheds.py
@@ -1,36 +1,33 @@
 import sys
-
-# Linux:
 sys.path.insert(0, '/home/shomea/a/anderovo/Dropbox/watershed/trapAnalysis/lib')
 sys.path.insert(0, '/home/shomea/a/anderovo/Dropbox/watershed/trapAnalysis/util')
-
 import load_geotiff
 import util
 import numpy as np
 import plot
 import trap_analysis
 
-# Linux:
+"""
+Plot the n nodes with most nodes terminating there
+"""
+
 file_name = '/home/shomea/a/anderovo/Dropbox/watershed/trapAnalysis/lib/anders_hoh.tiff'
 
+n_largest_watersheds = 20
 landscape = load_geotiff.get_landscape(file_name)
-
 downslope_neighbors = util.get_downslope_neighbors(landscape.num_of_nodes_x, landscape.num_of_nodes_y,
                                                    landscape.coordinates[0:landscape.total_number_of_nodes, 2])
 endpoints = trap_analysis.get_node_endpoints(landscape.num_of_nodes_x, landscape.num_of_nodes_y, downslope_neighbors)
 
 unique, counts = np.unique(endpoints, return_counts=True)
-#print 'hellooo'
-#freq = itemfreq(endpoints)  # unique, counts
-#unique = freq[:, 0]
-#counts = freq[:, 1]
-#print 'bye'
-index_in_unique = np.argmax(counts)
-all_nodes_lead_to = unique[index_in_unique]
+unique_counts = np.column_stack((unique, counts))
+unique_counts = unique_counts[np.argsort(unique_counts[:, 1])]
 
+indices = np.where(endpoints == unique_counts[-1, 0])[0]
 
-indices = np.where(endpoints == all_nodes_lead_to)
+for i in range(1, n_largest_watersheds):
+    indices = np.concatenate((indices, np.where(endpoints == unique_counts[-i - 1, 0])[0]))
 
 coordinates = landscape.coordinates[indices]
-plot.plot_local_minimums(coordinates)
 
+plot.plot_watersheds(coordinates)
--- trapAnalysis/lib/plot.py
+++ trapAnalysis/lib/plot.py
@@ -1,13 +1,26 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-def plot_landscape(grid):
-    # Create colormap
-    c = plt.get_cmap('terrain')
-    c.set_bad(color='k', alpha=0.0)
 
-    # Plot
-    plt.imshow(arr[:, 0:1000], cmap=c)
+def plot_landscape(landscape):
+    """
+    Plot the height of the landscape given a landscape object
+    :param landscape: Landscape object with all data
+    :return: Plot the landscape in the x-y-coordinate system
+    """
+    x_grid = np.linspace(landscape.x_min + landscape.step_size, landscape.x_max, landscape.num_of_nodes_x)
+    y_grid = np.linspace(landscape.y_max, landscape.y_min + landscape.step_size, landscape.num_of_nodes_y)
+    x, y = np.meshgrid(x_grid, y_grid)
+    z = landscape.arr
+
+    cmap = plt.get_cmap('terrain')
+    v = np.linspace(min(landscape.coordinates[:, 2]), max(landscape.coordinates[:, 2]), 100, endpoint=True)
+    plt.contourf(x, y, z, v, cmap=cmap)
+    plt.colorbar(label='Height', spacing='uniform')
+
+    plt.title('Height of the landscape')
+    plt.xlabel('x')
+    plt.ylabel('y')
     plt.show()
 
 
@@ -17,3 +30,9 @@ def plot_local_minimums(local_minimums_coordinates):
     plt.plot(local_minimums_coordinates[0::16, 0], local_minimums_coordinates[0::16, 1], 'ro')
     plt.show()
 
+
+def plot_watersheds(node_coordinates):
+
+    plt.plot(node_coordinates[:, 0], node_coordinates[:, 1], 'ro')
+    #plt.gca().invert_yaxis()
+    plt.show()
