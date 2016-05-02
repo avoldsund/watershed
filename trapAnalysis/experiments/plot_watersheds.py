0ade94c59fa62b3399088ed2984934e36bd7e60a
Anders Voldsund <andersvoldsund@gmail.com>
2016-05-02 15:26:11 +0200

    Only includes the necessary data file for plotting

--- trapAnalysis/experiments/plot_watersheds.py
+++ trapAnalysis/experiments/plot_watersheds.py
@@ -1,17 +1,29 @@
 import sys
+import numpy as np
+import cPickle
 sys.path.insert(0, '/home/shomea/a/anderovo/Dropbox/watershed/trapAnalysis/lib')
-sys.path.insert(0, '/home/shomea/a/anderovo/Dropbox/watershed/trapAnalysis/util')
+sys.path.insert(0, '/home/shomea/a/anderovo/Dropbox/watershedLargeFiles')
+
 import load_geotiff
-import trap_analysis
+import plot
 
-"""
-Get all watersheds by combining the minimums and the nodes having them as endpoints
-"""
-
+saved_files = '/home/shomea/a/anderovo/Dropbox/watershedLargeFiles/'
 file_name = '/home/shomea/a/anderovo/Dropbox/watershed/trapAnalysis/lib/anders_hoh.tiff'
 
 landscape = load_geotiff.get_landscape(file_name)
-watersheds = trap_analysis.get_watersheds(landscape.coordinates[:, 2], landscape.num_of_nodes_x,
-                                          landscape.num_of_nodes_y)
 
-print sorted(watersheds)
+"""
+ Test creation of the watershed. Load all files instead of redoing it every time.
+"""
+
+#downslope_neighbors = np.load(saved_files + 'downslopeNeighbors.npy')
+#endpoints = np.load(saved_files + 'endPoints.npy')
+
+#minimum_indices = np.where(downslope_neighbors == -1)[0]
+
+#minimums_in_each_watershed = cPickle.load(open(saved_files + 'minimumsInEachWatershed.pkl', 'rb'))
+#indices_leading_to_endpoints = cPickle.load(open(saved_files + 'indicesLeadingToEndpoints.pkl', 'rb'))
+#minimums_in_watersheds = cPickle.load(open(saved_files + 'minimumsInWatersheds.pkl', 'rb'))
+nodes_in_watersheds = cPickle.load(open(saved_files + 'nodesInWatersheds.pkl', 'rb'))
+
+plot.plot_watersheds(nodes_in_watersheds, landscape.num_of_nodes_x)
