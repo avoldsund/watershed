45e64cc4064ffd0fbf59fde2fea603e3f360748b
Anders Voldsund <andersvoldsund@gmail.com>
2016-05-07 17:50:29 +0200

    Testing the fit_data_in_landscape method

--- trapAnalysis/experiments/test_get_same_size.py
+++ trapAnalysis/experiments/test_get_same_size.py
@@ -1,1 +1,30 @@
-__author__ = 'anderovo'
+import sys
+import numpy as np
+import time
+import cPickle
+import networkx
+from networkx.algorithms.components.connected import connected_components
+from tempfile import TemporaryFile
+
+sys.path.insert(0, '/home/shomea/a/anderovo/Dropbox/watershed/trapAnalysis/lib')
+sys.path.insert(0, '/home/shomea/a/anderovo/Dropbox/watershed/trapAnalysis/util')
+sys.path.insert(0, '/home/shomea/a/anderovo/Dropbox/watershedLargeFiles')
+
+import load_geotiff
+import util
+import trap_analysis
+import time
+import plot
+
+saved_files = '/home/shomea/a/anderovo/Dropbox/watershedLargeFiles/'
+heights_file = '/home/shomea/a/anderovo/Dropbox/watershed/trapAnalysis/lib/anders_hoh.tiff'
+# lakes_file = '/home/shomea/a/anderovo/Dropbox/watershed/trapAnalysis/lib/anders_innsjo.tiff'
+# rivers_file = '/home/shomea/a/anderovo/Dropbox/watershed/trapAnalysis/lib/anders_elvbekk.tiff'
+small_rivers_file = '/home/shomea/a/anderovo/Dropbox/watershed/trapAnalysis/lib/anders_elvmidtlinje.tiff'
+# marshes_file = '/home/shomea/a/anderovo/Dropbox/watershed/trapAnalysis/lib/anders_myr.tiff'
+
+landscape = load_geotiff.get_landscape(heights_file)
+
+new_data = load_geotiff.fit_data_in_landscape(landscape, small_rivers_file)
+print np.shape(new_data)
+print new_data
