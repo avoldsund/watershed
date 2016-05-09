6dce9a957f2504a231a889ca9828b2bf0b59acff
Anders Voldsund <andersvoldsund@gmail.com>
2016-05-09 13:44:14 +0200

    Add method for plotting watersheds and lakes, rivers, marshes

---
+++ trapAnalysis/experiments/plot_watersheds_2D_alternative_and_information.py
@@ -0,0 +1,25 @@
+import sys
+import cPickle
+
+sys.path.insert(0, '/home/shomea/a/anderovo/Dropbox/watershed/trapAnalysis/lib')
+sys.path.insert(0, '/home/shomea/a/anderovo/Dropbox/watershed/trapAnalysis/util')
+sys.path.insert(0, '/home/shomea/a/anderovo/Dropbox/watershedLargeFiles')
+
+import load_geotiff
+import plot
+
+saved_files = '/home/shomea/a/anderovo/Dropbox/watershedLargeFiles/'
+heights_file = saved_files + 'anders_hoh.tiff'
+
+"""
+ Make watershed using information about which points in the landscape that are lakes and rivers.
+"""
+
+landscape = load_geotiff.get_landscape_tyrifjorden(heights_file)
+
+nodes_in_watersheds = cPickle.load(open(saved_files + 'nodesInWatershedsAlternative.pkl', 'rb'))
+print 'Total number of watersheds: ', len(nodes_in_watersheds)
+small_watersheds = ([watershed for watershed in nodes_in_watersheds if len(watershed) <= 100])
+print len(small_watersheds)
+
+plot.plot_watersheds_2d_alternative(nodes_in_watersheds, landscape, 1)
--- trapAnalysis/lib/plot.py
+++ trapAnalysis/lib/plot.py
@@ -124,8 +124,8 @@ def plot_watersheds_3d(nodes_in_watersheds, landscape, ds):
     # Construct the (x, y)-coordinate system
     x_grid = np.linspace(landscape.x_min, landscape.x_max, landscape.num_of_nodes_x)
     y_grid = np.linspace(landscape.y_max, landscape.y_min, landscape.num_of_nodes_y)
-    x_landscape, y_landscape = np.meshgrid(x_grid[0::2], y_grid[0::2])
-    z_landscape = landscape.arr[0::2, 0::2]
+    x_landscape, y_landscape = np.meshgrid(x_grid[0::ds], y_grid[0::ds])
+    z_landscape = landscape.arr[0::ds, 0::ds]
 
     # Plot the landscape in 3D
     fig, ax = plt.subplots(subplot_kw=dict(projection='3d'))
@@ -146,31 +146,6 @@ def plot_watersheds_3d(nodes_in_watersheds, landscape, ds):
         z = landscape.arr[row_col[0::ds, 0], row_col[0::ds, 1]]
 
         ax.scatter(x, y, z, c=next(colors), s=30, lw=0, zorder=1)
-
-    plt.show()
-
-
-def plot_combined_minimums(combined_minimums, landscape):
-    """
-    General plotting of minimums. Can decide how large the combinations must be.
-    :param combined_minimums: All the combinations of minimums in the landscape.
-    :param landscape: The landscape object.
-    :return: Plot all combined minimums with over n minimum nodes.
-    """
-
-    # Turning sets into arrays, and removing all combinations with less than n nodes
-    combined_minimums = [np.array(list(comb)) for comb in combined_minimums]
-    combined_minimums = [comb_min for comb_min in combined_minimums if len(comb_min) > 10]
-
-    # Plotting each combination (lake)
-    for lake in combined_minimums:
-        row_col = util.get_row_and_col_from_indices(lake, landscape.num_of_nodes_x)
-        plt.scatter(row_col[:, 1], row_col[:, 0], s=1, facecolor='0.5', lw=0)
-
-    # Fixing labels and axis
-    plt.xlabel('x')
-    plt.ylabel('y')
-    plt.gca().invert_yaxis()
 
     plt.show()
 
@@ -211,24 +186,49 @@ def plot_watersheds_2d_alternative(nodes_in_watersheds, landscape, ds):
         row_col = util.get_row_and_col_from_indices(large_watersheds[i], landscape.num_of_nodes_x)
         plt.scatter(landscape.x_min + row_col[0::ds, 1] * landscape.step_size,
                     landscape.y_max - row_col[0::ds, 0] * landscape.step_size,
-                    color=next(color_list), s=25, lw=0, alpha=0.5)
+                    color=next(color_list), s=25, lw=0, alpha=0.8)
 
     # Colors for the 10 largest watersheds
-    colors = iter(['lime', 'deeppink', 'chartreuse', 'fuchsia', 'aquamarine',
-                   'darkorchid', 'lawngreen', 'purple', 'darkgreen', 'indigo'])
+    colors = iter(['palevioletred', 'deeppink', 'pink', 'violet', 'aquamarine',
+                   'darkorchid', 'purple', 'lawngreen', 'seagreen', 'indigo'])
 
     # Plot the 10 largest watersheds using different colors
     for i in range(nr_of_large_watersheds-10, nr_of_large_watersheds):
         row_col = util.get_row_and_col_from_indices(large_watersheds[i], landscape.num_of_nodes_x)
         plt.scatter(landscape.x_min + row_col[0::ds, 1] * landscape.step_size,
                     landscape.y_max - row_col[0::ds, 0] * landscape.step_size,
-                    color=next(colors), s=25, lw=0, alpha=0.5)
+                    color=next(colors), s=25, lw=0, alpha=0.8)
 
     # Set title and labels
     plt.rcParams.update({'font.size': 14})
-    plt.title('All watersheds with over 10 nodes, using information about lakes, rivers and marshes')
+    plt.title('All watersheds with over 100 nodes, using information about lakes, rivers and marshes')
     plt.xlabel('x')
     plt.ylabel('y')
+
+    plt.show()
+
+
+def plot_combined_minimums(combined_minimums, landscape):
+    """
+    General plotting of minimums. Can decide how large the combinations must be.
+    :param combined_minimums: All the combinations of minimums in the landscape.
+    :param landscape: The landscape object.
+    :return: Plot all combined minimums with over n minimum nodes.
+    """
+
+    # Turning sets into arrays, and removing all combinations with less than n nodes
+    combined_minimums = [np.array(list(comb)) for comb in combined_minimums]
+    combined_minimums = [comb_min for comb_min in combined_minimums if len(comb_min) > 10]
+
+    # Plotting each combination (lake)
+    for lake in combined_minimums:
+        row_col = util.get_row_and_col_from_indices(lake, landscape.num_of_nodes_x)
+        plt.scatter(row_col[:, 1], row_col[:, 0], s=1, facecolor='0.5', lw=0)
+
+    # Fixing labels and axis
+    plt.xlabel('x')
+    plt.ylabel('y')
+    plt.gca().invert_yaxis()
 
     plt.show()
 
@@ -265,6 +265,84 @@ def plot_lakes_rivers_marshes(landscape, lakes, rivers, small_rivers, marshes):
     # Title and labels
     plt.rcParams.update({'font.size': 14})
     plt.title('Lakes, rivers and marshes')
+    plt.xlabel('x')
+    plt.ylabel('y')
+
+    plt.show()
+
+
+def plot_watersheds_and_lakes_rivers_marshes(landscape, nodes_in_watersheds, ds, lakes, rivers, small_rivers, marshes):
+    """
+    Plot all watersheds, as well as lakes, rivers and marshes from the data files in different colors.
+    :param landscape: The landscape object.
+    :param lakes: The indices for all lakes.
+    :param rivers: The indices for all rivers.
+    :param small_rivers: The indices for all small rivers.
+    :param marshes: The indices for all marshes.
+    :return: Plot the lakes, rivers and marshes.
+    """
+
+    # Construct the (x, y)-coordinate system
+    x_grid = np.linspace(landscape.x_min, landscape.x_max, landscape.num_of_nodes_x)
+    y_grid = np.linspace(landscape.y_max, landscape.y_min, landscape.num_of_nodes_y)
+    x, y = np.meshgrid(x_grid[0::ds], y_grid[0::ds])
+    z = landscape.arr[0::ds, 0::ds]
+
+    # Plot terrain in the background
+    cmap = plt.get_cmap('terrain')
+    v = np.linspace(min(landscape.coordinates[:, 2]), max(landscape.coordinates[:, 2]), 100, endpoint=True)
+    plt.contourf(x, y, z, v, cmap=cmap)
+    plt.colorbar(label='Height', spacing='uniform')
+
+    # Only plot watersheds with more than n nodes
+    large_watersheds = [watershed for watershed in nodes_in_watersheds
+                        if len(watershed) > 100]
+    large_watersheds.sort(key=len)
+    nr_of_large_watersheds = len(large_watersheds)
+
+    # The list of colors it cycles through
+    color_list = ['red', 'green', 'blue', 'yellow']
+    color_list = iter(color_list * (nr_of_large_watersheds/3))
+
+    # Plot all watersheds except the 10 largest
+    for i in range(nr_of_large_watersheds - 10):
+        row_col = util.get_row_and_col_from_indices(large_watersheds[i], landscape.num_of_nodes_x)
+        plt.scatter(landscape.x_min + row_col[0::ds, 1] * landscape.step_size,
+                    landscape.y_max - row_col[0::ds, 0] * landscape.step_size,
+                    color=next(color_list), s=25, lw=0, alpha=0.8)
+
+    # Colors for the 10 largest watersheds
+    colors = iter(['palevioletred', 'deeppink', 'pink', 'violet', 'aquamarine',
+                   'darkorchid', 'purple', 'lawngreen', 'seagreen', 'indigo'])
+
+    # Plot the 10 largest watersheds using different colors
+    for i in range(nr_of_large_watersheds-10, nr_of_large_watersheds):
+        row_col = util.get_row_and_col_from_indices(large_watersheds[i], landscape.num_of_nodes_x)
+        plt.scatter(landscape.x_min + row_col[0::ds, 1] * landscape.step_size,
+                    landscape.y_max - row_col[0::ds, 0] * landscape.step_size,
+                    color=next(colors), s=25, lw=0, alpha=0.8)
+
+    # Areas, legends and colors to cycle through.
+    areas = [lakes, rivers, small_rivers, marshes]
+    legends = ['Lakes', 'Large rivers', 'Small rivers', 'Marshes']
+    colors = ['darkblue', 'royalblue', 'lightskyblue', 'black']
+
+    for i in range(len(areas)):
+        row_col = util.get_row_and_col_from_indices(areas[i], landscape.num_of_nodes_x)
+        plt.scatter(landscape.x_min + row_col[0::, 1] * landscape.step_size,
+                    landscape.y_max - row_col[0::, 0] * landscape.step_size,
+                    color=colors[i], s=5, lw=0, alpha=1, label=legends[i])
+
+    # Making the legend for the lakes, rivers and marshes
+    lakes_patch = mpatches.Patch(color='darkblue', label='Lakes')
+    rivers_patch = mpatches.Patch(color='royalblue', label='Large rivers')
+    small_rivers_patch = mpatches.Patch(color='lightskyblue', label='Small rivers')
+    marshes_patch = mpatches.Patch(color='black', label='Marshes')
+    plt.legend(handles=[lakes_patch, rivers_patch, small_rivers_patch, marshes_patch])
+
+    # Title and labels
+    plt.rcParams.update({'font.size': 14})
+    plt.title('All watersheds with over 100 nodes, using information about lakes, rivers and marshes')
     plt.xlabel('x')
     plt.ylabel('y')
 
