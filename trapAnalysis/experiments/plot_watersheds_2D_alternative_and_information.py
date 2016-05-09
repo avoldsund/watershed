db0baea34952ce5db85bdce414f8107b3eae10c0
Anders Voldsund <andersvoldsund@gmail.com>
2016-05-09 13:46:34 +0200

    Add method for plotting all watersheds and the rivers, marshes and lakes

--- trapAnalysis/experiments/plot_watersheds_2D_alternative_and_information.py
+++ trapAnalysis/experiments/plot_watersheds_2D_alternative_and_information.py
@@ -7,19 +7,35 @@ sys.path.insert(0, '/home/shomea/a/anderovo/Dropbox/watershedLargeFiles')
 
 import load_geotiff
 import plot
+import numpy as np
 
 saved_files = '/home/shomea/a/anderovo/Dropbox/watershedLargeFiles/'
 heights_file = saved_files + 'anders_hoh.tiff'
-
+lakes_file = saved_files + 'anders_innsjo.tiff'
+rivers_file = saved_files + 'anders_elvbekk.tiff'
+small_rivers_file = saved_files + 'anders_elvmidtlinje.tiff'
+marshes_file = saved_files + 'anders_myr.tiff'
 """
  Make watershed using information about which points in the landscape that are lakes and rivers.
 """
 
 landscape = load_geotiff.get_landscape_tyrifjorden(heights_file)
-
+ds = 1
 nodes_in_watersheds = cPickle.load(open(saved_files + 'nodesInWatershedsAlternative.pkl', 'rb'))
-print 'Total number of watersheds: ', len(nodes_in_watersheds)
-small_watersheds = ([watershed for watershed in nodes_in_watersheds if len(watershed) <= 100])
-print len(small_watersheds)
 
-plot.plot_watersheds_2d_alternative(nodes_in_watersheds, landscape, 1)
+lakes, rivers, small_rivers, marshes = load_geotiff.get_lake_river_marsh_information_tyrifjorden(
+    landscape, lakes_file, rivers_file, small_rivers_file, marshes_file)
+
+lakes = lakes.flatten().astype(int)
+rivers = rivers.flatten().astype(int)
+small_rivers = small_rivers.flatten().astype(int)
+marshes = marshes.flatten().astype(int)
+
+lakes = np.where(lakes == 1)[0]
+rivers = np.where(rivers == 1)[0]
+small_rivers = np.where(small_rivers == 1)[0]
+marshes = np.where(marshes == 1)[0]
+
+
+plot.plot_watersheds_and_lakes_rivers_marshes(landscape, nodes_in_watersheds, ds,
+                                              lakes, rivers, small_rivers, marshes)
